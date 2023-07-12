0f97ef7f01326dd9f134e494b28a76ba0a083d2f
Shashank K S <shashank.s2@mphasis.com>
2023-07-12 15:16:39 +0530

    first commit

---
+++ Example03/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // TryParse method
+            string strInt = "123";
+            int i;
+            bool parsedSuccessfully = Int32.TryParse(strInt, out i);
+            if (parsedSuccessfully)
+            {
+                Console.WriteLine("The string \"{0}\" was successfully parsed as an integer: {1}", strInt, i);
+            }
+            else
+            {
+                Console.WriteLine("The string \"{0}\" could not be parsed as an integer.", strInt);
+            }
+
+            // Convert method
+            string strDouble = "123.456";
+            double d;
+            try
+            {
+                d = Convert.ToDouble(strDouble);
+                Console.WriteLine("The string \"{0}\" was successfully parsed as a double: {1}", strDouble, d);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The string \"{0}\" could not be parsed as a double.", strDouble);
+            }
+
+            // Parse method
+            string strDecimal = "123.456";
+            Decimal dec;
+            try
+            {
+                dec = Decimal.Parse(strDecimal);
+                Console.WriteLine("The string \"{0}\" was successfully parsed as a decimal: {1}", strDecimal, dec);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The string \"{0}\" could not be parsed as a decimal.", strDecimal);
+            }
+
+        }
+    }
+}
